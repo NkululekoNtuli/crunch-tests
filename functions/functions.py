3c4a5643233ca021b838db88f11fcf12b9a4838e
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-11-14 18:48:54 +0200

    complete

--- functions/functions.py
+++ functions/functions.py
@@ -3,11 +3,17 @@ STEP 1: Fix the declaration of the functions below to get the
 tests in test_functions.py to run.
 """
 
+
 def get_date_of_birth(id_number): 
     """
     STEP 2: Extract the date of birth from the ID number and return it as a string
     """
     #todo
+    year = id_number[4:6]
+    month = id_number[2:4] 
+    day = id_number[0:2]
+    return year + "/" + month + "/" + day
+
 def get_gender(id_number):
     """
     STEP 3: Extract the gender from the ID number using the formula below and return
@@ -17,6 +23,12 @@ def get_gender(id_number):
     female and if it is greater than 4999, the person is male.
     """
     #todo
+    if int(id_number[6]) >= 0 and int(id_number[6]) <= 4:
+        return "Female"
+    else:
+        return "Male"
+
+
 def get_citizenship(id_number):
     """
     STEP 4: Extract the citizenship from the ID number using the formula below and 
@@ -27,3 +39,8 @@ def get_citizenship(id_number):
     African citizen.
     """
     #todo
+
+    if int(id_number[10]) < 1:
+        return "South African"
+    else:
+        return "Non-South African"
